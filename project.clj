34297a598ab02e9c248e8168678613464de31f96
aria42 <me@aria42.com>
2010-11-16 10:40:23 -0800

    Changed opennlp dep

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject nlputil-clj "1.0-SNAPSHOT"
   :dependencies 
-    [[org.clojars.thnetos/opennlp-tools "1.4.3"]
-     [org.clojure/clojure "1.2.0"]
-     [org.clojure/clojure-contrib "1.2.0"]]
+  [[org.clojars.pjt/opennlp-tools "1.4.3"]
+    [org.clojure/clojure "1.2.0"]
+    [org.clojure/clojure-contrib "1.2.0"]]
    :dev-dependencies
    [[swank-clojure "1.2.1"]])
