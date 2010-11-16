1ecdccda623069db7a23402bc97c95f728f95a08
aria42 <me@aria42.com>
2010-11-16 02:32:24 -0500

    Changed groupname

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject aria42/nlputil-clj "1.0-SNAPSHOT"
+(defproject nlputil-clj "1.0-SNAPSHOT"
   :dependencies 
     [[org.clojars.thnetos/opennlp-tools "1.4.3"]
      [org.clojure/clojure "1.2.0"]
