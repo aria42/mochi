522f115dab754fd8a543233ce03625c38c951e60
aria42 <me@aria42.com>
2011-05-09 20:13:17 -0700

    updated plumbing deps

--- project.clj
+++ project.clj
@@ -3,10 +3,8 @@
   [[org.clojars.pjt/opennlp-tools "1.4.3"]
     [org.clojure/clojure "1.2.0"]
     [org.clojure/clojure-contrib "1.2.0"]
-    [clj-sys/plumbing "0.1.3-SNAPSHOT"]]
+    [clj-sys/plumbing "0.1.5-SNAPSHOT"]]
   :aot [mochi.nlp.process.tokenizer]
   :dev-dependencies [[swank-clojure "1.2.1"]]
-  :repositories  {"apache" "https://repository.apache.org/content/repositories/releases/"
-                  "snapshots" "http://mvn.getwoven.com/repos/woven-public-snapshots"
-                  "releases" "http://mvn.getwoven.com/repos/woven-public-releases"})
+  :repositories  {"apache" "https://repository.apache.org/content/repositories/releases/"})
    
